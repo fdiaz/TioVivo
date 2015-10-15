be0a79189421dae1269be476ff58ffb206eba77e
Francisco Diaz <fdiaz@users.noreply.github.com>
2015-10-15 11:35:39 -0300

    Add default demo project

---
+++ Tiovivo/Tiovivo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tiovivo
+//
+//  Created by Francisco Díaz on 10/15/15.
+//  Copyright © 2015 Francisco Díaz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
